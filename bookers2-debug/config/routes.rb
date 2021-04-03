59e69f5246ff547ff4d2f023df52e93c9f5ce96a
mikanfirst <mmkkmkk4869@gmail.com>
2021-04-03 08:03:44 +0000

    [start]課題3

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resources :post_comments,only: [:create, :destroy]
+  end
   resources :books
   root 'homes#top'
   get 'home/about' => 'homes#about'
