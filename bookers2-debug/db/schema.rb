b5af516144e11f34d9b465a0d9591aaac2bc2c65
mikanfirst <mmkkmkk4869@gmail.com>
2021-04-05 06:39:10 +0000

    [start]課題4

--- bookers2-debug/db/schema.rb
+++ bookers2-debug/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_145131) do
+ActiveRecord::Schema.define(version: 2021_04_05_063558) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,20 @@ ActiveRecord::Schema.define(version: 2021_04_04_145131) do
     t.text "comment"
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "followed_id"
+    t.integer "follower_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relatonships", force: :cascade do |t|
+    t.integer "followed_id"
+    t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
