98783f875d9aadb1aa6ddedc1e9f749f6df37a60
mikanfirst <mmkkmkk4869@gmail.com>
2021-04-07 03:06:09 +0000

    [add]課題4

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
+  post 'follow/:id' => 'relationships#follow', as: 'follow' #　フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' #フォローを外す
   resources :books do
     resource :favorites,only: [:create, :destroy]
     resources :post_comments,only: [:create, :destroy]
