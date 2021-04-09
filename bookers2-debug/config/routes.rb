af028f1c2b010e2caae7da19cde975be62e05ed6
mikanfirst <mmkkmkk4869@gmail.com>
2021-04-09 02:02:30 +0000

    [finish]課題5

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  get '/search', to: 'searchs#search', as: 'search'
 
   resources :users, only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
