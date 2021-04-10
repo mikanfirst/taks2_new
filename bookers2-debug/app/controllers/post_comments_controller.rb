ceeb30cc5615a5b82f3f609f38b4ad2fd563bfc7
mikanfirst <mmkkmkk4869@gmail.com>
2021-04-10 09:34:56 +0000

    [finish]課題6

--- bookers2-debug/app/controllers/post_comments_controller.rb
+++ bookers2-debug/app/controllers/post_comments_controller.rb
@@ -2,16 +2,17 @@ class PostCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id]) #本来(params[:id])→本のidを探してる?
+    @book = Book.find(params[:book_id]) #本来(params[:id])→本のidを探してる?
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
   end
 
   def destroy
-    PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    post_comment = @book.post_comments.find(params[:id])
+    post_comment.destroy
+    # PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
   end
 
   private
